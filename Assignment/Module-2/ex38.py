387d578f68ad911a10bb2f0bd86790e99eb2385c
Vivek <vivekvadodariya783@gmail.com>
2025-09-03 10:50:17 +0530

    Modified data...

--- Assignment/Module-2/ex38.py
+++ Assignment/Module-2/ex38.py
@@ -1,3 +1,9 @@
-import pywhatkit as pwk
+import pywhatkit as pwk, os
 
 pwk.sendwhatmsg_instantly("+918980268240", "Hello!")
+
+os.rename("PyWhatKit_DB.txt", "Temp_Data/PyWhatKit_DB.txt")
+
+# os library is used to rename or move file and save in particular folder.
+
+print("Successfully Sent...!!")
--- Assignment/Module-2/ex39.py
+++ Assignment/Module-2/ex39.py
@@ -3,7 +3,10 @@ from pytubefix import YouTube
 
 url = 'https://www.youtube.com/watch?v=0HyIda5eub8'
 
-yt = YouTube(url=url).streams.get_highest_resolution().download()
+yt = YouTube(url=url).streams.get_highest_resolution().download(output_path="Temp_Data")
 
 print("Successfully Downloaded...!!")
 
+# Temp_Data is relative path
+# when use this --> r"F:\Tops-Technology\Python_Vivek_Tops03\Assignment\Temp_Data" --> using raw string
+# then it will not give error for backslash
