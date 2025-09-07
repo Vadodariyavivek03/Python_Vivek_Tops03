33c225db8731a3077121cfbd72bddda0df95847f
Vivek <vivekvadodariya783@gmail.com>
2025-09-07 21:49:52 +0530

    Modified...

---
+++ Assignment/Module-2/ex40.py
@@ -0,0 +1,11 @@
+import mylib as ml
+
+ml.divide(10, 0)
+
+ml.power(5, 2)
+
+ml.subtract(5, 3)
+
+ml.add(10, 5)
+
+ml.multiply(2, 4)
---
+++ Assignment/Module-2/mylib.py
@@ -0,0 +1,22 @@
+def add(a, b):
+    print("Sum :",a+b)
+
+def subtract(a, b):
+    print("Sub :",a-b)    
+
+def multiply(a, b):
+    print("Mul :",a*b)
+
+def divide(a, b):   
+    
+    try:
+        print("Div :",a/b)
+       
+    except Exception as e:
+        print("Error :",e)
+
+def power(a, b):
+    print("Power :",a**b)
+
+
+
